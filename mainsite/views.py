8819b4786c6b01c3a68a476134a639478d1a52a4
Richard <skynet.tw@gmail.com>
2020-12-01 15:24:49 +0800

    first commit

---
+++ mainsite/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from mainsite.models import Post
+import random
+from datetime import datetime
+from mainsite.models import AccessInfo
+
+def homepage(request):
+    rec = AccessInfo()
+    rec.save()
+    hit_count = len(AccessInfo.objects.all())
+    posts = Post.objects.all()
+    now = datetime.now()
+    return render(request, "index.html", locals())
+
+def mychart(request):
+    now = datetime.now()
+    return render(request, "mychart.html", locals())
+
+def showpost(request, slug):
+	now = datetime.now()
+	try:
+		post = Post.objects.get(slug=slug)
+		return render(request, "post.html", locals())
+	except:
+		return redirect("/")
+
+def lotto(request):
+    lucky = random.randint(1, 42)
+    lottos = list()
+    for i in range(6):
+        lottos.append(random.randint(1, 42))
+    
+    return render(request, "lotto.html", locals())
+    
