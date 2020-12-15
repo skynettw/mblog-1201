5cee5768205b7b488b8701d61745f4a237eb2138
Richard <skynet.tw@gmail.com>
2020-12-15 15:19:05 +0800

    1215

--- mainsite/views.py
+++ mainsite/views.py
@@ -15,10 +15,33 @@ def homepage(request):
 
 def mychart(request, bid=0):
     now = datetime.now()
+
+    stores = Branch.objects.all()
+
     if bid==0:
         data = StoreIncome.objects.all()
     else:
         data = StoreIncome.objects.filter(branch=bid)
+    title = "各分店營收情形"
+    return render(request, "mychart.html", locals())
+
+def chart(request, year=0, month=0):
+    now = datetime.now()
+    stores = Branch.objects.all()
+
+    if year==0:
+        data = StoreIncome.objects.all()
+    else:
+        data = StoreIncome.objects.filter(income_year=year)
+        if month>0:
+            data = data.filter(income_month=month)
+
+    if year>0 and month>0:
+        title = "{}年{}月各分店營收情形".format(year, month)
+    elif year>0:
+        title = "{}年各分店營收情形".format(year)
+    else:
+        title = "各分店營收情形"
     return render(request, "mychart.html", locals())
 
 def showpost(request, slug):
--- mblog/urls.py
+++ mblog/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from mainsite.views import homepage, lotto, showpost, mychart
+from mainsite.views import homepage, lotto, showpost, mychart, chart
 
 
 urlpatterns = [
@@ -9,5 +9,7 @@ urlpatterns = [
     path('lotto/', lotto),
     path('mychart/', mychart),
     path('mychart/<int:bid>/', mychart),
+    path('chartbydate/<int:year>/<int:month>/', chart),
+    path('chartbydate/<int:year>/', chart),
     path('', homepage),
 ]
